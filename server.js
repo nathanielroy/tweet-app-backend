7090a4b6fd48519e4d0567f8760209067c850259
Nathaniel Roy <nathanielroy97@gmail.com>
2022-06-30 00:24:21 +0530

    cors

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const app = express();
 app.use('/api-docs',swaggerUI.serve,swaggerUI.setup(docs));
 
 var corsOptions = {
-  origin: "https://node-tweet-frontend.s3.amazonaws.com",
+  origin: "http://node-tweet-frontend.s3-website-us-east-1.amazonaws.com",
 };
 
 app.use(cors(corsOptions));
